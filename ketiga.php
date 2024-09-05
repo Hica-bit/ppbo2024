204668fe1344f900e469ed96d915efa473b7d6b6
Hica-bit <sachi.natta1430@gmail.com>
2024-09-05 22:22:02 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -4,12 +4,16 @@ class Lingkaran
     const PHI = 3.14;
     public $jari_jari;
 
-    public function luas($jari_jari) : float {
-        return self::PHI*$jari_jari*$jari_jari;
+    public function __construct($jari_jari)
+    {
+        $this->jari_jari = $jari_jari;
     }
-
-    public function keliling($jari_jari) : float {
-        return 2*self::PHI*$jari_jari;
+    public function luas($jari_jari) : float {
+        return self::PHI*$this->jari_jari*$this->jari_jari;
+    }
+    public function keliling(): float
+    {
+        return 2 * self::PHI * $this->jari_jari;
     }
 }
 
@@ -18,19 +22,31 @@ class Bola
     const PHI = 3.14;
     public $jari_jari;
 
-    public function volume($jari_jari) : float {
-        return (4/3)*self::PHI*pow($jari_jari,3);
+    public function __construct($jari_jari)
+    {
+        $this->jari_jari = $jari_jari;
     }
 
+    public function volume(): float
+    {
+        return (4/3) * self::PHI * $this->jari_jari * $this->jari_jari * $this->jari_jari;
+    }
 }
+
 class Tabung
 {
     const PHI = 3.14;
     public $jari_jari;
     public $tinggi;
 
-    public function volume($jari_jari,$tinggi) : float {
-        return self::PHI*pow($jari_jari,2)*$tinggi;
+    public function __construct($jari_jari, $tinggi)
+    {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+    public function volume(): float
+    {
+        return self::PHI * $this->jari_jari * $this->jari_jari * $this->tinggi;
     }
 }
 
@@ -40,27 +56,27 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
 
-    public function volume($jari_jari,$tinggi) : float {
-        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+    public function __construct($jari_jari, $tinggi)
+    {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
+    public function volume(): float
+    {
+        return (1/3) * self::PHI * $this->jari_jari * $this->jari_jari * $this->tinggi;
     }
 }
 
-$nasi_tumpeng = new Kerucut();
-$nasi_tumpeng->jari_jari = 4;
-$nasi_tumpeng->tinggi = 10;
+$nasi_tumpeng = new Kerucut(4,10);
 echo "Volume dari nasi tumpeng : " . $nasi_tumpeng->volume(4,10);
 
-$bundaran = new Lingkaran();
-$bundaran->jari_jari = 25;
+$bundaran = new Lingkaran(25);
 echo "\nLuas dari bundaran : " . $bundaran->luas(25);
 echo "\nKeliling dari bundaran : " . $bundaran->keliling(25);
 
-$pingpong = new Bola();
-$pingpong->jari_jari = 10;
+$pingpong = new Bola(10);
 echo "\nVolume pingpong : " . $pingpong->volume(10);
 
-$celengan = new Tabung();
-$celengan->jari_jari = 4;
-$celengan->tinggi = 16;
+$celengan = new Tabung(4, 16);
 echo "\nVolume celengan : " . $celengan->volume(4,16);
-
