f22a8422076a61ca5ae6ae7ad28f936004b63915
Hica-bit <sachi.natta1430@gmail.com>
2024-09-06 15:54:29 +0700

    Praktikum 5A - Memisahkan Class

---
+++ Tabung.php
@@ -0,0 +1,16 @@
+class Tabung
+{
+    const PHI = 3.14;
+    public $jari_jari;
+    public $tinggi;
+
+    public function __construct($jari_jari, $tinggi)
+    {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+    public function volume(): float
+    {
+        return self::PHI * $this->jari_jari * $this->jari_jari * $this->tinggi;
+    }
+}
