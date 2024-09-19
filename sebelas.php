ad6760a55a6ae3fa68965de3985d0a79e3b218fe
Hica-bit <sachi.natta1430@gmail.com>
2024-09-19 23:23:54 +0700

    Praktikum 6F - Membuat Objek Pewarisan

---
+++ sebelas.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'App/Admin/Pegawai.php';
+require_once 'App/Admin/Dosen.php';
+
+use App\Admin\Dosen;
+
+$dian = new Dosen();
+
+$dian->nama = "Dian Prawira";
+$dian->nip = 198411132015041001;
+$dian->setNoHp(62111111);
+$dian->alamat = "Jln Purnama";
+$dian->nidn = "0013118405";
+
+$dian->mengajar();
